d894e027b4d50d6b16d340a197531e04c0d7cc6b
Jasand <jas.sandmaster@gmail.com>
2017-05-25 16:29:42 -0700

    first portolio site deployment commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var express = require('express');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/static'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+ res.render('index');
+})
+
+app.listen(8000, function() {
+ console.log('listening on port 8000');
+})
+
+module.exports = app;
---
+++ static/js/parallax_functions.js
@@ -0,0 +1,52 @@
+$(window).scroll(function(){
+  var winScroll = $(this).scrollTop();
+  // console.log(winScroll);
+  $('.stars_iso').css({
+    'transform': 'translate(0px, -'+ winScroll/3280 +'%)'
+  });
+
+  $('.asteroids').css({
+    'transform': 'translate(0px, '+ winScroll/120 +'%)'
+  });
+
+  $('.tripp_figure').css({
+    'transform': 'translate(0px, '+ winScroll/240 +'%)'
+  });
+
+  $('.planets_one').css({
+    'transform': 'translate(0px, -'+ winScroll/480 +'%)'
+  });
+
+  $('.tripp_logo').css({
+    'transform': 'translate(0px, -'+ winScroll/960 +'%)'
+  });
+
+  $('.space_race').css({
+    'transform': 'translate(0px, -'+ winScroll/1920 +'%)'
+  });
+
+  $('.planets_two').css({
+    'transform': 'translate(0px, -'+ winScroll/3840 +'%)'
+  });
+
+  $('.asteroids2A').css({
+    'transform': 'translate(-'+ winScroll/24 +'%, -'+ winScroll/60 +'%)'
+  });
+
+  $('.asteroids2B').css({
+    'transform': 'translate('+ winScroll/24 +'%, -'+ winScroll/60 +'%)'
+  });
+
+  $('.asteroids3').css({
+    'transform': 'translate(0px, -'+ winScroll/20 +'%)'
+  });
+
+  if(winScroll > $('.gallery_images').offset().top - ($(window).height()/1.15)){
+    $('.gallery_images figure').each(function(i){
+      setTimeout(function(){
+        $('.gallery_images figure').eq(i).addClass('is-showing');
+      }, 150 * (i+1));
+    });
+  }
+
+});
